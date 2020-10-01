b8b81c9ca27639c2641f3cb481bb7d5dbf3393df
Erik <erik.daniel.nobleta@oocl.com>
2020-10-01 11:28:48 +0800

    Finished activity

---
+++ main.js
@@ -0,0 +1,18 @@
+let user = {
+    name : 'John',
+    surname : 'Mike'
+};
+console.log(user);
+user.name = 'Peter';
+console.log(user);
+delete user.name;
+console.log(user);
+
+
+
+let fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+console.log(fruit.apple + fruit.pear + fruit.peach);
